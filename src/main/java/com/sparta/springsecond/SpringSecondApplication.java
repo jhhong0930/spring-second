8a086f11325f1df1d7001967e17103975a8e7c69
jhhong0930 <jhhong0930@gmail.com>
2021-10-02 20:26:19 +0900

    create project spring-second

---
+++ src/main/java/com/sparta/springsecond/SpringSecondApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springsecond/SpringSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
