b344c6f91349c84808a628b7206cf7dcb638f136
jhhong0930 <jhhong0930@gmail.com>
2021-10-04 01:31:31 +0900

    add: 로그인, 로그아웃, 회원가입 기능 추가

---
+++ src/main/java/com/sparta/springsecond/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.sparta.springsecond.controller;
+
+import com.sparta.springsecond.dto.SignupRequestDto;
+import com.sparta.springsecond.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    // 회원 로그인 페이지
+    @GetMapping("/user/login")
+    public String login() {
+        return "login";
+    }
+
+    // 회원 가입 페이지
+    @GetMapping("/user/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    // 회원 가입 요청 처리
+    @PostMapping("/user/signup")
+    public String registerUser(SignupRequestDto requestDto) {
+        userService.registerUser(requestDto);
+        return "redirect:/user/login";
+    }
+
+}
