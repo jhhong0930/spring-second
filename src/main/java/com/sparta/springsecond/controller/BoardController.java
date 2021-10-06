6f8ce8454feed707455993326eb2abf50502968b
jhhong0930 <jhhong0930@gmail.com>
2021-10-07 03:14:01 +0900

    add: 게시글 상세 보기 기능 추가

--- src/main/java/com/sparta/springsecond/controller/BoardController.java
+++ src/main/java/com/sparta/springsecond/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -51,6 +52,19 @@ public class BoardController {
         boardService.register(requestDto, username);
 
         return "redirect:/";
+    }
+
+    // 게시글 상세보기 페이지
+    @GetMapping("/boards/{bno}")
+    public String read(@PathVariable Long bno, Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        if (userDetails != null) {
+            model.addAttribute("username", userDetails.getUsername());
+        }
+
+        model.addAttribute("board", boardService.read(bno));
+
+        return "board/read";
     }
 
 }
--- src/main/java/com/sparta/springsecond/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/springsecond/security/WebSecurityConfig.java
@@ -28,6 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/js/**").permitAll()
                 .antMatchers("/user/**").permitAll()
                 .antMatchers("/").permitAll()
+                .antMatchers("/boards/*").permitAll()
                 // 그 외 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
