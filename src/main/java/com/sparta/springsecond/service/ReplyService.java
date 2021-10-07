20008e23eab2e49c18ff0b38d25a8ac63f607ba9
jhhong0930 <jhhong0930@gmail.com>
2021-10-07 10:45:01 +0900

    update: 기능 수정

--- src/main/java/com/sparta/springsecond/service/ReplyService.java
+++ src/main/java/com/sparta/springsecond/service/ReplyService.java
@@ -1,11 +1,14 @@
 package com.sparta.springsecond.service;
 
+import com.sparta.springsecond.domain.Board;
 import com.sparta.springsecond.domain.Reply;
 import com.sparta.springsecond.dto.ReplyRequestDto;
+import com.sparta.springsecond.repository.BoardRepository;
 import com.sparta.springsecond.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class ReplyService {
 
     private final ReplyRepository replyRepository;
+    private final BoardRepository boardRepository;
 
     // 댓글 전체 조회
     public List<Reply> getList(Long bno) {
@@ -21,9 +25,21 @@ public class ReplyService {
     }
 
     // 댓글 작성
+    @Transactional
     public void register(ReplyRequestDto requestDto, String username, Long bno) {
 
         Reply reply = new Reply(requestDto, username, bno);
+
+        Board board = boardRepository.findById(bno)
+                .orElseThrow(() -> new NullPointerException("해당 게시글을 찾을수 없습니다."));
+
+        if (board != null) {
+            replyRepository.save(reply);
+            board.addReply(reply);
+            boardRepository.save(board);
+        }
+
+
 
         replyRepository.save(reply);
     }
@@ -42,7 +58,21 @@ public class ReplyService {
     }
 
     // 댓글 삭제
+    @Transactional
     public void delete(Long rno) {
+
+        Reply reply = replyRepository.findById(rno)
+                .orElseThrow(() -> new NullPointerException("해당 댓글이 존재하지 않습니다."));
+
+        Long bno = reply.getBno();
+
+        Board board = boardRepository.findById(bno)
+                .orElseThrow(() -> new NullPointerException("해당 게시글을 찾을수 없습니다."));
+
+        List<Reply> replies = board.getReplies();
+
+        replies.remove(reply);
+
         replyRepository.deleteById(rno);
     }
 
