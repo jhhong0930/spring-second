53515d3e488bb50f31e1cc623aec4576ea4c321b
jhhong0930 <jhhong0930@gmail.com>
2021-10-09 09:15:09 +0900

    add: 타임존 추가

--- src/main/java/com/sparta/springsecond/SpringSecondApplication.java
+++ src/main/java/com/sparta/springsecond/SpringSecondApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringSecondApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecondApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
